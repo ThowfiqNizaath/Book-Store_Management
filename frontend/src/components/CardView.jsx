dd0d555a91f3fecfca2fa335f00b29283ef8e69a
ThowfiqNizaath <smartnizath@gmail.com>
2024-12-20 10:34:05 +0530

    cardView updated

--- frontend/src/components/CardView.jsx
+++ frontend/src/components/CardView.jsx
@@ -7,9 +7,9 @@ const CardView = ({books}) => {
   return (
     <>
       <div className="grid max-xs:gap-y-4 sm:grid-cols-[1fr_1fr] gap-y-4 lg:grid-cols-[1fr_1fr_1fr] xl:grid-cols-[1fr_1fr_1fr_1fr]  w-full md:px-2 py-2 gap-x-5">
-        {books.map((book) => (
+        {books ? books.map((book) => (
           <SingleBookCard book={book} key={book._id} />
-        ))}
+        )): ''}
       </div>
     </>
   );
